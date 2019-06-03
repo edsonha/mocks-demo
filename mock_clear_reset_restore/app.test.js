8d4b79aa2ae7684605f1c00396f8512059542559
Angeline <atan@thoughtworks.com>
2019-06-03 15:43:38 +0800

    Add example showing difference between clear, reset and restore

---
+++ mock_clear_reset_restore/app.test.js
@@ -0,0 +1,40 @@
+const app = require("./app");
+const math = require("./math");
+
+test("example on mockRestore", () => {
+  const addMock = jest.spyOn(math, "add");
+
+  // override the implementation
+  addMock.mockImplementation(() => "mock");
+  expect(app.doAdd(1, 2)).toEqual("mock");
+
+  // restore the original implementation
+  addMock.mockRestore();
+  expect(app.doAdd(1, 2)).toEqual(3);
+});
+
+test("example on mockClear", () => {
+  math.add = jest.fn(() => "add");
+
+  expect(app.doAdd(1, 2)).toEqual("add");
+  expect(app.doAdd(1, 2)).toEqual("add");
+  expect(math.add).toHaveBeenCalledTimes(2);
+
+  // Clears the number of times called retains mock implementation
+  math.add.mockClear();
+  expect(app.doAdd(1, 2)).toEqual("add");
+  expect(math.add).toHaveBeenCalledTimes(1);
+});
+
+test("example on mockReset", () => {
+  math.add = jest.fn(() => "add");
+
+  expect(app.doAdd(1, 2)).toEqual("add");
+  expect(app.doAdd(1, 2)).toEqual("add");
+  expect(math.add).toHaveBeenCalledTimes(2);
+
+  // Clears the number of times called as well as mock implementation
+  math.add.mockReset();
+  expect(app.doAdd(1, 2)).toEqual(undefined);
+  expect(math.add).toHaveBeenCalledTimes(1);
+});
